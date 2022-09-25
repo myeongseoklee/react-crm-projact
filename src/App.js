acb2b0baf5f44a254557da81cf9a9f778e85baa4
MS-Leeffice <yhkyhk92@gmail.com>
2022-09-26 03:42:15 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
-import React, { Component } from 'react';
+import {Paper, Table, TableBody, TableCell, TableHead, TableRow} from '@mui/material/';
+// import { styled } from '@mui/material/styles'
+
+// const styles = theme => ({
+//   root: {
+//     width: '100%',
+//     marginTop: theme.spacing.unit * 3,
+//     overflowX: "auto"
+//   },
+//   table: {
+//     minWidth: 1080
+//   }
+// })
 
 const customers = [{
   'id' : 1,
@@ -28,12 +40,28 @@ const customers = [{
   'job' : '인사총무'
 }
 ]
+
 class App extends Component {
   render() {
+    // const { classes } = this.props;
     return (
-      <div>
-          {
-            customers.map(c => {
+      <Paper sx={{overflowX: "auto"}}>
+        <Table stickyHeader sx={{maxWidth: '1080px', minWidth: '1080px'}}>
+      {/* <Paper className={classes.root} sx={{maxHeight:'300px'}}>
+        <Table className={classes.table} > */}
+          <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+          </TableHead>
+
+          <TableBody>
+          {customers.map(c => {
               return (
               <Customer
                 key={c.id}
@@ -43,12 +71,11 @@ class App extends Component {
                 birthday={c.birthday}
                 gender={c.gender}
                 job={c.job}
-                />)
-            })
-          }
-        </div>
-      );
-  }
+                />)})}
+          </TableBody>
+        </Table>
+      </Paper>
+     )}
 }
 
 export default App;
